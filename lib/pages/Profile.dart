64aa7be458e7e5ab0dcca554cbd3b08c7ca97de9
MuthamaFrancis <francisfrancs02@gmail.com>
2024-03-19 19:45:20 +0300

    Profile update

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter_application_1/configs/constants.dart';
 
 class Profile extends StatelessWidget {
@@ -9,7 +9,7 @@ class Profile extends StatelessWidget {
       appBar: AppBar(
         title: const Center(child: Text('Profile')),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -19,69 +19,23 @@ class Profile extends StatelessWidget {
                 backgroundColor: whitecolor,
                 child: Icon(Icons.person, color: primaryColor, size: 150.0),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text("Francis Musau",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Phone :  ",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: "+254769892579",
-                          //recognizer: TapGestureRecognizer()
-                          //  ..onTap = () {
-                          //    //_launchURL("mailto:musaifrancis@gmail.com?subject=News&body=New%20plugin");
-                          //  },
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 20.0),
-                  Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Icon(
-                      Icons.edit,
-                      color: primaryColor,
-                    ),
-                  )
-                ],
-              ),
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Email: ",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextSpan(
-                      text: "musaufrancis@gmail.com",
-                      //recognizer: TapGestureRecognizer()
-                      //  ..onTap = () {
-                      //    //_launchURL("mailto:musaifrancis@gmail.com?subject=News&body=New%20plugin");
-                      //  },
-                    ),
-                  ],
+              SizedBox(height: 20.0),
+              Text(
+                "Francis Musau",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
                 ),
+              ),
+              SizedBox(height: 20.0),
+              InfoCard(
+                title: "Phone :",
+                subtitle: "+254769892579",
+              ),
+              SizedBox(height: 20.0),
+              InfoCard(
+                title: "Email:",
+                subtitle: "musaufrancis@gmail.com",
               ),
             ],
           ),
@@ -91,7 +45,47 @@ class Profile extends StatelessWidget {
   }
 }
 
-class InfoCard {
-  String? title;
-  InfoCard({required String? title});
+class InfoCard extends StatelessWidget {
+  final String? title;
+  final String? subtitle;
+
+  InfoCard({required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title!,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              subtitle!,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          GestureDetector(
+            onTap: () {
+              // Handle edit action
+            },
+            child: Icon(
+              Icons.edit,
+              color: primaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
